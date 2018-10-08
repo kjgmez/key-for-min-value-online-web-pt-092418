6d5ee07be6f9769ad803093a29c3704670862214
Kevin J Gomez <kjgmez1@gmail.com>
2018-10-08 22:54:42 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,10 @@ def key_for_min_value(name_hash)
 
   def key_for_min_value(name_hash)
     smallest_name = nil
-    smallest = 1000000
+    smallest = 0
 
     name_hash.each do |key, value|
-      if value <= smallest
+      if value >= smallest || value == 0
         smallest = value
         smallest_name = key
       end
